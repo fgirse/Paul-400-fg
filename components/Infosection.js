5d56f6130d5c2890ae1a4dc6924e67a6bb66c0d0
fgirse <fgirse@bluewin.ch>
2022-03-10 02:32:30 +0100

    initial 2 . commit

--- components/Infosection.js
+++ components/Infosection.js
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
 import LogoNeu from '../data/SvgLogoNeu'
-import { PhoneIcon } from '@heroicons/react/solid'
-import { AtSymbolIcon } from '@heroicons/react/solid'
+import { PhoneIcon } from 'heroicons/react/'
+import { AtSymbolIcon } from 'heroicons/react'
 import Modal6 from './Modal6.js'
 
 function Infosection() {
